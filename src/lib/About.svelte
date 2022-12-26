e3114e4fba291dc89a734d17d91f07be86d6c013
Michael-Andrew Keays <michaelandrewkeays@gmail.com>
2022-12-25 23:56:53 -0500

    concept 1 WIP

---
+++ src/lib/About.svelte
@@ -0,0 +1,45 @@
+<section id="about" class="flex flex-col gap-4 min-h-screen">
+    <div class="my-auto px-4">
+        <div class="flex">
+            <h2 class="text-6xl text-right text-white">Who am I</h2>
+            <h2 class="text-6xl text-right text-blue-700">?</h2>
+            <h2 class="text-6xl text-right text-blue-500">?</h2>
+            <h2 class="text-6xl text-right text-blue-300">?</h2>
+        </div>
+        <p class="text-2xl w-2/3 text-white">
+            Passionate technologist, enthusiastic learner, strong communicator,
+            and experienced leader. Excited to take on new opportunities and
+            challanges. I have spent time building skills across several
+            different software industries since I was young. I am at my best
+            when trailblazing, and enjoy working as both a self-starter, a team
+            member, and also as a mentor. While I focus myself as a developer, I
+            love working with all parts of the software cycle: From management,
+            to quality, to marketing, user experience, and all parts in between.
+        </p>
+    </div>
+</section>
+
+<style>
+    #about {
+        background-image: linear-gradient(
+            315deg,
+            rgb(164, 34, 147) 0%,
+            hsl(288deg 91% 49%) 11%,
+            hsl(284deg 86% 47%) 22%,
+            hsl(280deg 83% 44%) 33%,
+            hsl(276deg 82% 41%) 44%,
+            hsl(272deg 82% 37%) 56%,
+            hsl(267deg 83% 33%) 67%,
+            hsl(261deg 84% 29%) 78%,
+            hsl(253deg 87% 25%) 89%,
+            hsl(237deg 100% 20%) 100%
+        );
+        /*
+        background-color: black;
+        background-attachment: fixed;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    */
+    }
+</style>
---
+++ src/lib/Hero.svelte
@@ -0,0 +1,79 @@
+<svelte:head>
+    <title>Michael-Andrew Keays</title>
+</svelte:head>
+
+<section id="home" class="h-screen w-full">
+    <div class="flex flex-col px-4 gap-2 sm:gap-0 mr-auto">
+        <h1 class="text-left text-6xl sm:text-8xl">Michael</h1>
+        <div class="backdrop-invert">
+            <h1
+                class="text-left text-6xl sm:text-8xl invert text-shadow italic -ml-3"
+            >
+                Andrew
+            </h1>
+        </div>
+        <h1 class="text-left text-6xl sm:text-8xl">Keays</h1>
+    </div>
+    <div class="w-full flex flex-col sm:flex-row justify-between sm:px-6">
+        <h2>Software Engineer</h2>
+        <div class="flex flex-row gap-4 sm:gap-12">
+            <a href="#about">About</a>
+            <a href="#experience">Experience</a>
+            <a href="#skills">Skills</a>
+            <a href="/resume">Resume</a>
+        </div>
+    </div>
+</section>
+
+<style>
+    @import url("https://fonts.googleapis.com/css2?family=Michroma&family=Righteous&family=Titillium+Web:wght@600&family=Unbounded&display=swap");
+
+    section {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        flex: 1;
+    }
+
+    #home {
+        background-image: linear-gradient(
+            45deg,
+            hsl(237deg 100% 20%) 0%,
+            hsl(253deg 87% 25%) 11%,
+            hsl(261deg 84% 29%) 22%,
+            hsl(267deg 83% 33%) 33%,
+            hsl(272deg 82% 37%) 44%,
+            hsl(276deg 82% 41%) 56%,
+            hsl(280deg 83% 44%) 67%,
+            hsl(284deg 86% 47%) 78%,
+            hsl(288deg 91% 49%) 89%,
+            hsl(292deg 100% 50%) 100%
+        );
+        /*
+        background-attachment: fixed;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        */
+    }
+
+    h1 {
+        width: 100%;
+        color: white;
+        font-family: "Michroma";
+        padding: 0;
+    }
+
+    h2 {
+        color: white;
+    }
+
+    a {
+        color: white;
+    }
+
+    .text-shadow {
+        text-shadow: 2px 0 #0008;
+    }
+</style>
