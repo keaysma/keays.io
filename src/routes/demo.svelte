0727bed8d8ec56f86a71bd63d6c01b8d204c133c
Michael-Andrew Keays <michaelandrewkeays@gmail.com>
2022-12-27 18:18:38 -0500

    fix build issues

--- src/routes/demo.svelte
+++ src/routes/demo.svelte
@@ -13,10 +13,6 @@
 <section>
 	<h1>
 		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
 		</div>
 
 		to your new<br />SvelteKit app
