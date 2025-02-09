0196fab63c796a45c7ea337b25b03b701a04ddd2
Michael-Andrew Keays <michaelandrewkeays@gmail.com>
2025-02-09 17:07:38 -0500

    Add 3rd party icons, revise skills

--- src/lib/content/Experience.svelte
+++ src/lib/content/Experience.svelte
@@ -2,6 +2,7 @@
     import { FontAwesomeIcon } from "fontawesome-svelte";
     import {
         faPython,
+        faGolang,
         faReact,
         faNodeJs,
         faJava,
@@ -12,6 +13,7 @@
         faDocker,
         faUnity,
     } from "@fortawesome/free-brands-svg-icons";
+    import type { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 
     import { certs, experiences } from "$lib/store";
 
@@ -19,38 +21,65 @@
     import WorkCard from "$lib/work-card.svelte";
     import CertCard from "$lib/cert-card.svelte";
 
-    const skillIcons = [
-        faPython,
-        faReact,
-        // NextJS
-        faNodeJs,
-        faJava,
-        // Ruby
-        faRust,
-        // C++
-        faGit,
-        faJenkins,
-        // Postgres
-        // Mongo
-        faAws,
-        // GCP
-        // K8s
-        faDocker,
-        faUnity,
+    import typescript from "$lib/icons/typescript.svg?raw";
+    import vue from "$lib/icons/vue.js.svg?raw";
+    import postgres from "$lib/icons/postgres.svg?raw";
+    import svelte from "$lib/icons/svelte.svg?raw";
+    import kubernetes from "$lib/icons/kubernetes.svg?raw";
+    import eks from "$lib/icons/eks.svg?raw";
+
+    type Icon =
+        | {
+              type: "fa";
+              icon: IconDefinition;
+          }
+        | {
+              type: "raw";
+              icon: string;
+              alt: string;
+          };
+    const icons: Icon[] = [
+        { type: "fa", icon: faPython },
+        { type: "fa", icon: faGolang },
+        { type: "fa", icon: faRust },
+        { type: "fa", icon: faJava },
+        { type: "raw", icon: typescript, alt: "typescript" },
+        { type: "fa", icon: faNodeJs },
+        { type: "fa", icon: faReact },
+        { type: "raw", icon: vue, alt: "Vue.Js" },
+        { type: "raw", icon: svelte, alt: "Svelte" },
+        { type: "raw", icon: postgres, alt: "postgres" },
+        { type: "fa", icon: faDocker },
+        { type: "raw", icon: kubernetes, alt: "K8s" },
+        { type: "raw", icon: eks, alt: "EKS" },
+        { type: "fa", icon: faAws },
+        { type: "fa", icon: faUnity },
     ];
 </script>
 
 <Section id="experience" format="block">
     <h2 class="bar">Skills & Tech</h2>
-    {#each skillIcons as icon}
-        <FontAwesomeIcon {icon} class="skill svg-md" />
+    {#each icons as { type, icon, ...details }}
+        {#if type === "fa"}
+            <figure>
+                <FontAwesomeIcon {icon} class="svg-lg" />
+                <figcaption>
+                    <i>{(icon as IconDefinition).iconName}</i>
+                </figcaption>
+            </figure>
+        {:else}
+            <figure>
+                {@html icon}
+                <figcaption>{(details as { alt: string }).alt}</figcaption>
+            </figure>
+        {/if}
     {/each}
     <ul>
-        <li>System Design and Implementation Execution</li>
-        <li>Full Stack Web and Mobile Development, Responsive Design</li>
+        <li>Feature & Project Ownership: Capacity Planning, Risk Assessment</li>
+        <li>System Design, Implementation, and Execution</li>
+        <li>Full Stack Web Dev: Responsive Design, Scalable Services, and Database Architecture</li>
         <li>Continuous Integration and End-to-End Test Automation</li>
         <li>Cross Functional Team Work</li>
-        <li>Agile Development & Scrum</li>
     </ul>
 
     <h2 class="bar">Work</h2>
@@ -59,17 +88,34 @@
     {/each}
 
     <h2 class="bar">School & Certs</h2>
-    <br><br>
+    <br /><br />
     {#each certs as cert}
         <CertCard {...cert} />
     {/each}
 </Section>
 
-<style>
+<style lang="scss">
     h2 {
         /* This just provides a nice little bit of space when going to the #experiences header */
         /* Also, this provides spacing between the sub-sections nicely, too */
         padding-top: 3rem;
+    }
+
+    figure {
+        display: inline-block;
+        font-size: x-large;
+        line-height: 1em;
+
+        margin: 0.75rem 0 0.5rem;
+        &:not(:last-of-type) {
+            margin-right: 1rem;
+        }
+
+        figcaption {
+            text-align: center;
+            font-size: small;
+            text-transform: capitalize;
+        }
     }
 
     ul {
